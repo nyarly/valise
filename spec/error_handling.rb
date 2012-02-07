77767bf1ff039a7749d347d6da8c86215e402cba
Judson <nyarly@gmail.com>
2012-02-06 17:22:14 -0800

    Creating pages

--- spec/error_handling.rb
+++
@@ -1,9 +0,0 @@
-require 'valise'
-
-describe Valise, "error conditions" do
-  describe "HandlerCollision is raised" do
-    it "when a file handler collides from the same path"
-    it "when a file handler collides with a glob path"
-    it "when two Valises are added with a handler for the same path"
-  end
-end
